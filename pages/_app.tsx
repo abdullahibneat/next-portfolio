05c8bc75e29ffd7dcf624ca4be1eeccbb4424680
Abdullah Ibne Atiq <hello@abdu.io>
2021-03-04 15:05:37 +0000

    Reset CSS, use Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
+import "../styles/global.css"
 import { AppProps } from "next/app"
+import Head from "next/head"
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps}></Component>
+const App = ({ Component, pageProps }: AppProps) => <>
+    <Head>
+        {/* Inter font */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps}></Component>
+</>
 
 export default App
