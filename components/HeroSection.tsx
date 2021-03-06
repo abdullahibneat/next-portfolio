57bf51059a5403d0a03612a19bd130bd19b8c6bb
Abdullah Ibne Atiq <hello@abdu.io>
2021-03-06 14:57:04 +0000

    Simplify HeroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,12 +1,10 @@
 import { FunctionComponent } from "react"
 import styles from "../styles/HeroSection.module.css"
-import Section, { SectionProps } from "./Section"
+import { SectionProps } from "./Section"
 
 const HeroSection: FunctionComponent<SectionProps> = ({ children, className = "" }) => {
     return <section className={`${styles.heroSection} ${className}`}>
-        <Section>
-            {children}
-        </Section>
+        {children}
     </section>
 }
 
