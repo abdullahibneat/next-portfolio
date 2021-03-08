ab184954ab391160bbd506bd091b5e70c86af021
Abdullah Ibne Atiq <hello@abdu.io>
2021-03-08 16:00:56 +0000

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,17 @@ import styles from "../styles/Home.module.css"
 
 const Home = () => <>
     <HeroSection className={styles.hero}>
-        <Section className={styles.content}>
+        <Section className={styles.heroContent}>
             <div className={styles.heroText}>
                 Consectetur adipiscing elit. Mauris in est scelerisque, scelerisque risus vel, tincidunt dolor.
             </div>
-            <Swiper zDistance={150} className={styles.browsers}>
-                <Browser className={styles.browser}>1</Browser>
-                <Browser className={styles.browser}>2</Browser>
-                <Browser className={styles.browser}>3</Browser>
-            </Swiper>
+            <div className={styles.content}>
+                <Swiper zDistance={150} className={styles.swiper}>
+                    <Browser className={styles.browser}>1</Browser>
+                    <Browser className={styles.browser}>2</Browser>
+                    <Browser className={styles.browser}>3</Browser>
+                </Swiper>
+            </div>
         </Section>
     </HeroSection>
 </>
