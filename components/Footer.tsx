5ed8a4b076f5627c0a30e3aa34fae8b5467fcfe4
Abdullah Ibne Atiq <hello@abdu.io>
2021-03-28 12:29:12 +0100

    Add contact section to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,22 @@
 import { FunctionComponent } from "react"
+import Section from "./Section"
+import Link from "next/link"
 import styles from "../styles/Footer.module.css"
 
-const Footer: FunctionComponent = () => <footer className={styles.footer}>
-    <div className={styles.container}>
-        <p>&copy; Abdullah Ibne Atiq</p>
-        <div className={styles.footerLinks}>
-            <a className={styles.link} href="#">GitHub</a> <span>•</span> <a className={styles.link} href="#">Contact</a>
+const Footer: FunctionComponent = () => <>
+    <Section className={styles.contact}>
+        <h2>💬 Get in touch</h2>
+        <p>Do you have a project in mind? Or need help in your company? If so, feel free to contact me.</p>
+        <Link href="#"><button>Let's talk →</button></Link>
+    </Section>
+    <footer className={styles.footer}>
+        <div className={styles.container}>
+            <p>&copy; Abdullah Ibne Atiq</p>
+            <div>
+                <Link href="#">GitHub</Link> <span>•</span> <Link href="#">Contact</Link>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</>
 
 export default Footer
