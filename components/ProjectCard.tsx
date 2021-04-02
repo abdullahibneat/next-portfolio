8f2d5e9826e4f3fc957b2d208f54ac34237dd255
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-02 19:45:17 +0100

    Add svg icons, update style

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -11,16 +11,27 @@ export type ProjectCardProps = {
 }
 
 const ProjectCard: FunctionComponent<ProjectCardProps> = ({ title, featuredImage = `https://source.unsplash.com/random/500x500?nature&random=${Math.random() * 50}`, url, github = undefined, live = undefined }) =>
-    <Link href={url}>
-        <a className={styles.card} style={{ backgroundImage: `url(${featuredImage})` }}>
-            <div className={styles.content}>
-                <h2><span>{title}</span></h2>
-                <div className={styles.links}>
-                    {github && <Link href={github}><a title="View source on GitHub" style={{ backgroundColor: "red" }} /></Link>}
-                    {live && <Link href={live}><a title="Visit this project" style={{ backgroundColor: "blue" }} /></Link>}
+    <div className={styles.wrapper}>
+        <Link href={url}>
+            <a className={styles.card} style={{ backgroundImage: `url(${featuredImage})` }}>
+                <div className={styles.content}>
+                    <h2><span>{title}</span></h2>
                 </div>
-            </div>
-        </a>
-    </Link>
+            </a>
+        </Link>
+        {/* The following icons are from https://feathericons.com/ */}
+        <div className={styles.links}>
+            {github && <Link href={github}>
+                <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                </a>
+            </Link>}
+            {live && <Link href={live}>
+                <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
+                </a>
+            </Link>}
+        </div>
+    </div>
 
 export default ProjectCard
