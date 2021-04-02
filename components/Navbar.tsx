de39779e4cf404b94ce23b044f33015c1c0426c9
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-02 15:00:49 +0100

    Fix pathname check

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <div className={styles.logo}>LOGO</div>
         <div className={`${styles.links} ${open? styles.mobile : ""}`}>
             <Link href="/"><a className={pathname === "/"? styles.currentPage : ""}>Home</a></Link>
-            <Link href="/projects"><a className={pathname === "projects"? styles.currentPage : ""}>Projects</a></Link>
-            <Link href="/contact"><a className={pathname === "contact"? styles.currentPage : ""}>Contact</a></Link>
+            <Link href="/projects"><a className={pathname === "/projects"? styles.currentPage : ""}>Projects</a></Link>
+            <Link href="/contact"><a className={pathname === "/contact"? styles.currentPage : ""}>Contact</a></Link>
             <div className={styles.mobileToggle}>
                 <span onClick={() => setOpen(!open)}>{open? "✖" : "☰"}</span>
             </div>
