adbc975e650a143b9d432f202646c4703eaa55c0
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-02 20:15:50 +0100

    Display sample projects in a grid layout

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,21 @@
-import Section from "../components/Section";
+import Section from "../components/Section"
+import ProjectCard from "../components/ProjectCard"
+import styles from "../styles/Projects.module.css"
+
+const projects = [
+    { title: "Lorem ipsum dolor sit amet consectetur adipisicing elit", url: "#" },
+    { title: "Lorem ipsum dolor sit amet consectetur adipisicing elit", url: "#", github: "https://github.com" },
+    { title: "Autem, quis natus perspiciatis vero facilis repellendus", url: "#", live: "https://abdu.io" },
+    { title: "Lorem ipsum dolor sit amet consectetur adipisicing elit", url: "#", github: "https://github.com", live: "https://abdu.io" },
+]
 
 const Projects = () => <Section>
-    <h1>Projects</h1>
+    <h1 className={styles.title}>Projects</h1>
+    <div className={styles.grid}>
+        {projects.map((p, i) =>
+            <ProjectCard key={i} {...p} />
+        )}
+    </div>
 </Section>
 
 export default Projects
