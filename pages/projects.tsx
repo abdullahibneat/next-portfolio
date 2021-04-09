1b1090d598ea4229e2c9260105d1dcdc6d43b0b9
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-09 15:45:29 +0100

    Add project filtering

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,21 +2,41 @@ import Section from "@components/Section"
 import ProjectCard from "@components/ProjectCard"
 import sanityClient from "../sanityClient"
 import styles from "@styles/Projects.module.css"
-import { FunctionComponent } from "react"
+import { FunctionComponent, useState } from "react"
 import { Project } from "types"
 
 type props = {
     projects: Project[]
 }
 
-const Projects: FunctionComponent<props> = ({ projects }) => <Section>
-    <h1 className={styles.title}>Projects</h1>
-    <div className={styles.grid}>
-        {projects.map((p, i) =>
-            <ProjectCard key={i} url={`/projects/${p.slug}`} {...p} />
-        )}
-    </div>
-</Section>
+const getCategories = (projects: Project[]) => {
+    const categories = projects.reduce((cats, p) => {
+        p.categories.forEach(c => cats.add(c))
+        return cats
+    }, new Set(["All"]))
+    return Array.from(categories)
+}
+
+const Projects: FunctionComponent<props> = ({ projects }) => {
+    const [categories] = useState(getCategories(projects))
+    const [currentCategory, setCategory] = useState("All")
+
+    return <Section>
+        <h1 className={styles.title}>Projects</h1>
+        <div className={styles.categories}>
+            {categories.map((c, i) =>
+                <span key={i} onClick={() => setCategory(c)} className={currentCategory === c? styles.current : ""}>{c}</span>
+            )}
+        </div>
+        <div className={styles.grid}>
+            {projects
+                .filter(p => currentCategory === "All" || p.categories.includes(currentCategory))
+                .map((p, i) =>
+                    <ProjectCard key={i} url={`/projects/${p.slug}`} {...p} />
+            )}
+        </div>
+    </Section>
+}
 
 export const getStaticProps = async () => {
     const projects = await sanityClient.fetch(`
@@ -25,7 +45,8 @@ export const getStaticProps = async () => {
             "slug": slug.current,
             "featuredImage": featuredImage.asset->url,
             github,
-            live
+            live,
+            "categories": categories[]->name
         }
     `)
     return {
