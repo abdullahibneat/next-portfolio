ced329883277532e99945748e41b637408c5aae5
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-09 15:17:30 +0100

    Create types.ts, add and use Project type

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,27 +1,28 @@
 import Section from "@components/Section"
-import ProjectCard, { ProjectCardProps } from "@components/ProjectCard"
+import ProjectCard from "@components/ProjectCard"
 import sanityClient from "../sanityClient"
 import styles from "@styles/Projects.module.css"
 import { FunctionComponent } from "react"
+import { Project } from "types"
 
 type props = {
-    projects: ProjectCardProps[]
+    projects: Project[]
 }
 
 const Projects: FunctionComponent<props> = ({ projects }) => <Section>
     <h1 className={styles.title}>Projects</h1>
     <div className={styles.grid}>
         {projects.map((p, i) =>
-            <ProjectCard key={i} {...p} />
+            <ProjectCard key={i} url={`/projects/${p.slug}`} {...p} />
         )}
     </div>
 </Section>
 
 export const getStaticProps = async () => {
     const projects = await sanityClient.fetch(`
-        *[_type == 'project'] {
+        *[_type == "project"] {
             title,
-            "url": "/projects/" + slug.current,
+            "slug": slug.current,
             "featuredImage": featuredImage.asset->url,
             github,
             live
---
+++ types.ts
@@ -0,0 +1,10 @@
+export type Project = {
+    title: string,
+    slug: string,
+    categories: string[],
+    featuredImage?: string,
+    github?: string,
+    live?: string,
+    summary: string,
+    body: string
+}
