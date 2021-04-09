1add4d4fb8275444ea2e29b95a657a6de65d40d7
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-09 12:26:06 +0100

    Create client to interact with Sanity

---
+++ sanityClient.js
@@ -0,0 +1,9 @@
+import sanityClient from "@sanity/client"
+const sanityJSON = require("./sanity/sanity.json")
+
+export default sanityClient({
+    // Retrieve the projectID from the Sanity studio config file
+    projectId: sanityJSON.api.projectId,
+    dataset: "production",
+    useCdn: process.env.NODE_ENV === "production"
+})
