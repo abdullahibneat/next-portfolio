bbd84465215de84c40306f92aaa433aa19ec4104
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-10 11:41:14 +0100

    Set Sanity API version

--- sanityClient.js
+++ sanityClient.js
@@ -5,5 +5,6 @@ export default sanityClient({
     // Retrieve the projectID from the Sanity studio config file
     projectId: sanityJSON.api.projectId,
     dataset: "production",
-    useCdn: process.env.NODE_ENV === "production"
+    useCdn: process.env.NODE_ENV === "production",
+    apiVersion: "2021-04-10"
 })
