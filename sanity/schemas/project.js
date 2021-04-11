5c36720d98da09596c60c9429f780ee3d8c8d660
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-11 10:56:37 +0100

    Add "quote" type to project schema

--- sanity/schemas/project.js
+++ sanity/schemas/project.js
@@ -70,6 +70,27 @@ export default {
                             isHighlighted: true
                         }
                     }]
+                },
+                {
+                    name: "quote",
+                    type: "object",
+                    fields: [
+                        {
+                            name: "quote",
+                            type: "text",
+                            validation: Rule => Rule.required()
+                        },
+                        {
+                            name: "author",
+                            type: "string",
+                            validation: Rule => Rule.required()
+                        },
+                        {
+                            name: "title",
+                            type: "string",
+                            validation: Rule => Rule.required()
+                        }
+                    ]
                 }
             ]
         }
