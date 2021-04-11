14cdadab83bbb298a5fadc23f1ed00de84936ca5
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-11 11:12:57 +0100

    Add className prop to QuoteBox

--- components/QuoteBox.tsx
+++ components/QuoteBox.tsx
@@ -6,9 +6,10 @@ export type Quote = {
     author: string
 }
 
-const QuoteBox: FunctionComponent<Quote> = ({ text, author }) => <div className={styles.container}>
-    <div className={styles.quote} dangerouslySetInnerHTML={{ __html: text }}></div>
-    <div className={styles.author} dangerouslySetInnerHTML={{ __html: author }}></div>
-</div>
+const QuoteBox: FunctionComponent<Quote & { className?: string }> = ({ className = "", text, author }) =>
+    <div className={`${styles.container} ${className}`}>
+        <div className={styles.quote} dangerouslySetInnerHTML={{ __html: text }}></div>
+        <div className={styles.author} dangerouslySetInnerHTML={{ __html: author }}></div>
+    </div>
 
 export default QuoteBox
