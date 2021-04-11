8b1a7452063466b441c5bd147e5bacc6dd5a64e0
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-11 11:23:51 +0100

    Add optional title field, update styles

--- components/QuoteBox.tsx
+++ components/QuoteBox.tsx
@@ -3,13 +3,15 @@ import styles from "@styles/QuoteBox.module.css"
 
 export type Quote = {
     text: string,
-    author: string
+    author: string,
+    title?: string
 }
 
-const QuoteBox: FunctionComponent<Quote & { className?: string }> = ({ className = "", text, author }) =>
+const QuoteBox: FunctionComponent<Quote & { className?: string }> = ({ className = "", text, author, title }) =>
     <div className={`${styles.container} ${className}`}>
         <div className={styles.quote} dangerouslySetInnerHTML={{ __html: text }}></div>
         <div className={styles.author} dangerouslySetInnerHTML={{ __html: author }}></div>
+        {title && <div className={styles.title} dangerouslySetInnerHTML={{ __html: title }}></div>}
     </div>
 
 export default QuoteBox
