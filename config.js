411f1ced021de499ceafe89a05e01ae0c233336f
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-15 13:23:28 +0100

    Create a configuration file

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    url: "https://abdu.io",
+    name: "Abdullah Ibne Atiq",
+    description: "My name is Abdullah, and I'm a computer science undergraduate student. Having worked with HTML/CSS/JavaScript, React, NodeJS, MongoDB and external APIs, I aspire to become a full stack developer.",
+    github: "abdullahibneat",
+    twitter: "@abdullahibneat",
+    defaultImage: "https://via.placeholder.com/1280x720.png?text=Default+image"
+}
