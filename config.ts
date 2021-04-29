bbb538c472775bf34973b051cd3daa7e9ae26fda
Abdullah Ibne Atiq <hello@abdu.io>
2021-04-29 12:59:42 +0100

    Load Home  props from config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,29 @@
+import { HomeProps } from "pages"
+
+const home: HomeProps = {
+    heroText: `<small style="font-weight: 400; font-size: 1rem">👋 Hello world!</small><br/>I'm Abdullah, a software developer from London who loves crafting modern experiences for the web.`,
+    skills: {
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dignissimos a! Sed nisi laborum porro asperiores veniam earum recusandae nesciunt incidunt vitae aliquid minus omnis dolorum, iste beatae voluptas dolor!",
+        skills: [
+            { name: "Full Stack Development", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
+            { name: "Java and Kotlin Development", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
+            { name: "Databases", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
+            { name: "Git", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" }
+        ]
+    },
+    testimonials: [
+        { author: "Adam Smith", text: "Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Corrupti mollitia voluptatibus <strong>aliquam</strong> maiores, nesciunt dolorum!" },
+        { author: "John Smith", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quis natus perspiciatis vero facilis repellendus?" },
+        { author: "John Doe", text: "Lorem ipsum dolor sit amet consectetur <strong>adipisicing</strong> elit. Autem, quis natus perspiciatis vero facilis repellendus?" },
+    ]
+}
+
 export default {
     url: "https://abdu.io",
     name: "Abdullah Ibne Atiq",
     description: "My name is Abdullah, and I'm a computer science undergraduate student. Having worked with HTML/CSS/JavaScript, React, NodeJS, MongoDB and external APIs, I aspire to become a full stack developer.",
     github: "abdullahibneat",
     twitter: "@abdullahibneat",
-    defaultImage: "https://via.placeholder.com/1280x720.png?text=Default+image"
+    defaultImage: "https://via.placeholder.com/1280x720.png?text=Default+image",
+    home
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,27 +7,24 @@ import Section from "@components/Section"
 import Skills, { Skill } from "@components/Skills"
 import Swiper from "@components/Swiper"
 import styles from "@styles/Home.module.css"
+import { FunctionComponent } from "react"
+import { GetStaticProps } from "next"
+import config from "config"
 
-const skills: Skill[] = [
-    { name: "Full Stack Development", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
-    { name: "Java and Kotlin Development", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
-    { name: "Databases", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" },
-    { name: "Git", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nobis?" }
-]
+export type HomeProps = {
+    heroText: string,
+    skills: {
+        text: string,
+        skills: Skill[]
+    },
+    testimonials: Quote[]
+}
 
-const testimonials: Quote[] = [
-    { author: "Adam Smith", text: "Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Corrupti mollitia voluptatibus <strong>aliquam</strong> maiores, nesciunt dolorum!" },
-    { author: "John Smith", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quis natus perspiciatis vero facilis repellendus?" },
-    { author: "John Doe", text: "Lorem ipsum dolor sit amet consectetur <strong>adipisicing</strong> elit. Autem, quis natus perspiciatis vero facilis repellendus?" },
-]
-
-const Home = () => <>
+const Home: FunctionComponent<HomeProps> = ({ heroText, skills, testimonials }) => <>
     <Meta title="Home" />
     <HeroSection className={styles.hero}>
         <Section className={styles.heroContent}>
-            <div className={styles.heroText}>
-                Consectetur adipiscing elit. Mauris in est scelerisque, scelerisque risus vel, tincidunt dolor.
-            </div>
+            <div className={styles.heroText} dangerouslySetInnerHTML={{ __html: heroText }}/>
             <div className={styles.heroProjects}>
                 <Swiper shakeFirst zDistance={150} className={styles.swiper}>
                     <Browser className={styles.browser}>1</Browser>
@@ -39,15 +36,18 @@ const Home = () => <>
         </Section>
     </HeroSection>
     <Section className={styles.skilsSection}>
-        <Skills skills={skills} className={styles.skillsBox} />
+        <Skills skills={skills.skills} className={styles.skillsBox} />
         <div className={styles.skillsContent}>
             <h1>Skills</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dignissimos a! Sed nisi laborum porro asperiores veniam earum recusandae nesciunt incidunt vitae aliquid minus omnis dolorum, iste beatae voluptas dolor!</p>
+            <p>{skills.text}</p>
         </div>
     </Section>
     <Carousel style={{ marginBottom: "5rem" }}>
         {testimonials.map((q, i) => <QuoteBox key={i} {...q} />)}
     </Carousel>
 </>
+
+// Load props from config file
+export const getStaticProps: GetStaticProps<HomeProps> = async _ => ({ props: config.home })
 
 export default Home
