c348009c9fb9c92959f952c5555b1a0f87a3a038
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-04 12:25:17 +0100

    Add function to get single project

--- services/projects.ts
+++ services/projects.ts
@@ -10,3 +10,13 @@ export const getProjects: () => Promise<Project[]> = async () => {
     `) as Project[]
     return projects
 }
+
+export const getProject: (slug: string) => Promise<Project> = async slug => {
+    const project = await sanityClient.fetch(`
+        *[_type == "project" && slug.current == $slug][0] {
+            "slug": slug.current,
+            ...
+        }
+    `, { slug })
+    return project
+}
