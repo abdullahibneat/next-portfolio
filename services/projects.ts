b9a4c3a5bcfd1f08b1c343cff6b94681f2029e75
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-04 17:19:25 +0100

    Set featuredImage to be direct url

--- services/projects.ts
+++ services/projects.ts
@@ -10,7 +10,8 @@ export const getProjects: (options?: GetterOptions) => Promise<Project[]> = asyn
         *[_type == "project" ${options.featured? "&& featured" : ""}] | order(_createdAt desc) {
             ...,
             "slug": slug.current,
-            "categories": categories[]->name
+            "categories": categories[]->name,
+            "featuredImage": featuredImage.asset->url
         }
     `) as Project[]
     return projects
@@ -21,7 +22,8 @@ export const getProjectBySlug: (slug: string) => Promise<Project> = async slug =
         *[_type == "project" && slug.current == $slug][0] {
             ...,
             "slug": slug.current,
-            "categories": categories[]->name
+            "categories": categories[]->name,
+            "featuredImage": featuredImage.asset->url
         }
     `, { slug }) as Project
     return project
--- types.ts
+++ types.ts
@@ -2,23 +2,9 @@ export type Project = {
     title: string,
     slug: string,
     categories: string[],
-    featuredImage?: SanityImage,
+    featuredImage?: string,
     github?: string,
     live?: string,
     summary: string,
     body: any
 }
-
-export type SanityImage = {
-    _type: string,
-    alt?: string,
-    asset: {
-        _type: string,
-        _ref: string
-    }
-}
-
-/**
- * Refer to https://www.sanity.io/docs/image-urls#BhPyF4m0
- */
-export type ImageOptions = { [key: string]: string | number }
