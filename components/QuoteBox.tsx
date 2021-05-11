72b38ac74d1e5af1c4f9751f7264e7ccb903ca1c
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-11 15:34:14 +0100

    Parse quote text with BlockContent

--- components/QuoteBox.tsx
+++ components/QuoteBox.tsx
@@ -1,11 +1,17 @@
 import { FunctionComponent } from "react"
 import styles from "@styles/QuoteBox.module.css"
 import { Quote } from "types"
+import BlockContent from "@sanity/block-content-to-react"
+import sanityClient from "@sanityClient"
 
 const QuoteBox: FunctionComponent<Quote & { className?: string }> = ({ className = "", quote, author, title }) =>
     <div className={`${styles.container} ${className}`}>
-        <div className={styles.quote} dangerouslySetInnerHTML={{ __html: quote }}></div>
-        <div className={styles.author} dangerouslySetInnerHTML={{ __html: author }}></div>
+        <div className={styles.quote}>
+            <BlockContent
+                blocks={quote}
+                {...sanityClient.config()} />
+        </div>
+        <p className={styles.author}>{author}</p>
         {title && <div className={styles.title} dangerouslySetInnerHTML={{ __html: title }}></div>}
     </div>
 
