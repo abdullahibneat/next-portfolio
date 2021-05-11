55c09d26b11f9595b67d40531572a55dd02641ce
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-11 11:46:40 +0100

    Add svg icon field

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -2,9 +2,19 @@ export default {
     name: "category",
     title: "Categories",
     type: "document",
-    fields: [{
-        name: "name",
-        type: "string",
-        validation: Rule => Rule.required()
-    }]
+    fields: [
+        {
+            name: "name",
+            type: "string",
+            validation: Rule => Rule.required()
+        },
+        {
+            name: "icon",
+            type: "image",
+            options: {
+                accept: "image/svg+xml"
+            },
+            validation: Rule => Rule.required()
+        }
+    ]
 }
