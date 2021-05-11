a97806dca434b61471c82c8992b735a4d9d71007
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-11 15:50:09 +0100

    Add "featured projects" field

--- sanity/schemas/homeSettings.js
+++ sanity/schemas/homeSettings.js
@@ -11,6 +11,11 @@ export default {
             validation: Rule => Rule.required()
         },
         {
+            name: "featuredProjects",
+            type: "array",
+            of: [{ type: "reference", to: [{ type: "project" }] }]
+        },
+        {
             name: "skillsText",
             type: "array",
             of: [{ type: "block" }],
