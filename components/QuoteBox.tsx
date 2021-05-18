7922df04cd7fb94a821a177866b934fbfa3117c0
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-18 12:38:39 +0100

    Fix quotes not wrapping properly

--- components/QuoteBox.tsx
+++ components/QuoteBox.tsx
@@ -4,11 +4,20 @@ import { Quote } from "types"
 import BlockContent from "@sanity/block-content-to-react"
 import sanityClient from "@sanityClient"
 
+// For some reason, BlockContent uses "&nbsp;" for spaces in quotes.
+// This utility funciton converts them back to normal spaces.
+const transformText = block => {
+    return {
+        ...block,
+        children: block.children.map(c => ({ ...c, text: c.text.replace(/\s/g, " ") }))
+    }
+}
+
 const QuoteBox: FunctionComponent<Quote & { className?: string }> = ({ className = "", quote, author, jobTitle }) =>
     <div className={`${styles.container} ${className}`}>
         <div className={styles.quote}>
             <BlockContent
-                blocks={quote}
+                blocks={quote.map(q => transformText(q))}
                 {...sanityClient.config()} />
         </div>
         <p className={styles.author}>{author}</p>
