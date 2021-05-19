c959c7abf3a8053005d240dcae4c35dfa7539493
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-19 12:20:15 +0100

    Install and configure SVGR

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ module.exports = {
     future: { webpack5: true },
     webpack: config => {
         config.experiments = { topLevelAwait: true }
+        
+        // Enable SVGR
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
+          })
         return config
     },
 }
