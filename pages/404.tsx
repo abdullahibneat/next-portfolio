d516cadad2ddb566f9920769d3df1a2497bc9b6c
Abdullah Ibne Atiq <hello@abdu.io>
2021-05-27 12:59:00 +0100

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Meta from "@components/Meta"
+import Section from "@components/Section"
+import Link from "next/link"
+
+export default () =>
+    <Section style={{ flex: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Meta title="Page not found" />
+        <h1>Error 404</h1>
+        <div style={{ margin: "2rem 0", textAlign: "center" }}>
+            <p style={{ marginBottom: "1rem" }}>This page could not be found. Perhaps the content moved to another location.</p>
+            <p>If you are sure something should have been here, please <Link href="/contact"><a style={{ color: "var(--primary-color)" }}>contact me</a></Link>.</p>
+        </div>
+        <Link href="/"><button>Return to homepage</button></Link>
+    </Section>
