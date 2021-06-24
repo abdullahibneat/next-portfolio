cea0f8d3a8050a69ab3746263eff12c68451a6e2
Abdullah Ibne Atiq <hello@abdu.io>
2021-06-24 11:43:36 +0100

    Remove unused @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -12,12 +12,6 @@ module.exports = {
     future: { webpack5: true },
     webpack: config => {
         config.experiments = { topLevelAwait: true }
-        
-        // Enable SVGR
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack']
-          })
         return config
     },
     env: {
