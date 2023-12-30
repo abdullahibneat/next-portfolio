beff260c2c0554efbefd315ca0fda95d28bb2826
Abdullah Ibne Atiq <hello@abdu.io>
2023-12-30 14:13:34 +0000

    Fix warning about font loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,16 @@
-import { AppProps } from "next/app"
-import Head from "next/head"
-import Meta from "@components/Meta"
-import Navbar from "@components/Navbar"
-import Footer from "@components/Footer"
-import "@styles/global.css"
+import { AppProps } from 'next/app'
+import Meta from '@components/Meta'
+import Navbar from '@components/Navbar'
+import Footer from '@components/Footer'
+import '@styles/global.css'
 
-const App = ({ Component, pageProps }: AppProps) => <>
-    <Head>
-        {/* Inter font */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-    </Head>
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
     <Meta />
     <Navbar />
     <Component {...pageProps}></Component>
     <Footer />
-</>
+  </>
+)
 
 export default App
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,25 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
 class Document extends NextDocument {
-    render() {
-        return <Html lang="en-GB">
-            <Head />
-            <body>
-                <div id="modal-root" />
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    }
+  render() {
+    return (
+      <Html lang="en-GB">
+        <Head>
+          {/* Inter font */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <div id="modal-root" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default Document
